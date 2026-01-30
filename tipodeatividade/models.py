49c0b692af1a2ac85fe66ffbacce6fc485aea4f5
micheleespacodosaber-cmyk <micheleespacodosaber@gmail.com>
2026-01-29 21:48:41 -0300

    janeiro

--- tipodeatividade/models.py
+++ tipodeatividade/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class tipodeatividade(models.Model):
+    """ Modelo representando um tipo de atividade"""
+    codigo = models.IntegerField(primary_key=True,
+                                 help_text='código do tipo de atividade')
+    descricao = models.CharField(max_length=70, null=False,
+                                 help_text='Informe a descrição do tipo de atividade')
+    
+
+    def _str_(self):
+        return f'{self.codigo} {self.descricao}'
